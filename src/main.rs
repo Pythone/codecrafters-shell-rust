c13e0803bebcc92d376a59c04b7d664fb79f3bab
wonder <msjhar@live.com>
2024-05-24 16:58:57 -0400

    shelltest

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,6 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    let command = input.trim();
+    println!("{command}: command not found");
 }
