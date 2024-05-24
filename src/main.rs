11c2a43464c11ea98bcf57419bb888bc637c0476
wonder <msjhar@live.com>
2024-05-24 17:01:06 -0400

    shelltest

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
     // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    let command = input.trim();
-    println!("{command}: command not found");
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let command = input.trim();
+        println!("{command}: command not found");
+    }
 }
